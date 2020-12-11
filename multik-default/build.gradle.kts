37a48e8232d3c945fe1f6154e2ab3ec6b097acd9
devcrocod <devcrocod@gmail.com>
2020-12-11 20:31:57 +0300

    [fix:] refactoring fat jar creation for multik-default

--- multik-default/build.gradle.kts
+++ multik-default/build.gradle.kts
@@ -4,20 +4,12 @@ dependencies {
     implementation(project(":multik-native"))
 }
 
-val fatJar = task("fatJar", type = Jar::class) {
+tasks.jar {
     manifest {
         attributes(
             mapOf("Implementation-Title" to project.name,
                 "Implementation-Version" to project.version)
         )
     }
-    manifest {
-        attributes["Implementation-Title"] = "Default implementation"
-        attributes["Implementation-Version"] = archiveVersion
-    }
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
 }
-
-tasks.named("build") { dependsOn(fatJar) }
-tasks.named("bintrayUpload") { dependsOn(fatJar) }
