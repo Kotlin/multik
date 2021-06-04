571d36606129d4c0fc550eb088c90b10308ba499
therealansh <tyagiansh23@hotmail.com>
2021-06-04 15:53:14 +0530

    review: append() for 1D array

--- multik-api/src/main/kotlin/org/jetbrains/kotlinx/multik/ndarray/operations/Add.kt
+++ multik-api/src/main/kotlin/org/jetbrains/kotlinx/multik/ndarray/operations/Add.kt
@@ -6,20 +6,99 @@ package org.jetbrains.kotlinx.multik.ndarray.operations
 
 import org.jetbrains.kotlinx.multik.ndarray.data.*
 
-public inline fun <reified T : Number> D1Array<T>.append(n: D1Array<T>): D1Array<T>{
+public fun <T : Number> D1Array<T>.append(n: D1Array<T>): D1Array<T>{
     val totalSize :Int = this.size+n.size
-    var dest = arrayOfNulls<T>(totalSize)
-    System.arraycopy(Array<T>(this.size) { this.data[it] }, 0,dest, 0, this.size)
-    System.arraycopy(Array<T>(n.size){n.data[it]}, 0, dest,this.size, n.size)
-    var appendedArr:D1Array<T> = D1Array<T>(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
-    return appendedArr
+    when (this.dtype) {
+        DataType.IntDataType -> {
+            var dest = IntArray(totalSize)
+            System.arraycopy(this.data.getIntArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getIntArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.FloatDataType -> {
+            var dest = FloatArray(totalSize)
+            System.arraycopy(this.data.getFloatArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getFloatArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.DoubleDataType -> {
+            var dest = DoubleArray(totalSize)
+            System.arraycopy(this.data.getDoubleArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getDoubleArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.LongDataType -> {
+            var dest = LongArray(totalSize)
+            System.arraycopy(this.data.getLongArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getLongArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.ShortDataType -> {
+            var dest = ShortArray(totalSize)
+            System.arraycopy(this.data.getShortArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getShortArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.ByteDataType -> {
+            var dest = ByteArray(totalSize)
+            System.arraycopy(this.data.getByteArray(),0,dest,0,this.size)
+            System.arraycopy(n.data.getByteArray(), 0, dest,this.size, n.size)
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+    }
+
 }
 
-public inline fun <reified T : Number> D1Array<T>.append(n: T): D1Array<T>{
+public fun <T : Number> D1Array<T>.append(n: T): D1Array<T>{
     val totalSize :Int = this.size+1
-    var dest = arrayOfNulls<T>(totalSize)
-    System.arraycopy(Array<T>(this.size) { this.data[it] }, 0,dest, 0, this.size)
-    dest[totalSize-1] = n
-    var appendedArr:D1Array<T> = D1Array<T>(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
-    return appendedArr
+    when (this.dtype) {
+        DataType.IntDataType -> {
+            var dest = IntArray(totalSize)
+            System.arraycopy(this.data.getIntArray(),0,dest,0,this.size)
+            dest[this.size] = n as Int
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.FloatDataType -> {
+            var dest = FloatArray(totalSize)
+            System.arraycopy(this.data.getFloatArray(),0,dest,0,this.size)
+            dest[this.size] = n as Float
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.DoubleDataType -> {
+            var dest = DoubleArray(totalSize)
+            System.arraycopy(this.data.getDoubleArray(),0,dest,0,this.size)
+            dest[this.size] = n as Double
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.LongDataType -> {
+            var dest = LongArray(totalSize)
+            System.arraycopy(this.data.getLongArray(),0,dest,0,this.size)
+            dest[this.size] = n as Long
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.ShortDataType -> {
+            var dest = ShortArray(totalSize)
+            System.arraycopy(this.data.getShortArray(),0,dest,0,this.size)
+            dest[this.size] = n as Short
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+        DataType.ByteDataType -> {
+            var dest = ByteArray(totalSize)
+            System.arraycopy(this.data.getByteArray(),0,dest,0,this.size)
+            dest[this.size] = n as Byte
+            var appendedArr:D1Array<T> = D1Array(data = initMemoryView<T>(totalSize, this.dtype){dest[it] as T},dim=D1, shape = intArrayOf(totalSize), dtype = this.dtype)
+            return appendedArr
+        }
+    }
 }
