6e6d25f5eac8c29e795358f6da076fb0d23c55fd
Nikolay.Smirnov <nikolay.smirnov@jetbrains.com>
2021-07-07 13:25:31 +0300

    Create multik-cuda module
    Implement dot function for matrices and vectors
    Add tests for dot function

---
+++ multik-cuda/src/main/kotlin/org/jetbrains/kotlinx/multik/cuda/CudaEngine.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.kotlinx.multik.cuda
+
+import jcuda.jcublas.JCublas
+import org.jetbrains.kotlinx.multik.api.*
+
+
+public class CudaEngineProvider : EngineProvider {
+    override fun getEngine(): Engine? {
+        return CudaEngine
+    }
+}
+
+public object CudaEngine : Engine(), AutoCloseable {
+    override val name: String
+        get() = type.name
+
+    override val type: EngineType
+        get() = CudaEngineType
+
+    init {
+        JCublas.cublasInit()
+    }
+
+    override fun close() {
+        JCublas.cublasShutdown()
+    }
+
+    override fun getMath(): Math {
+        return CudaMath
+    }
+
+    override fun getLinAlg(): LinAlg {
+        return CudaLinAlg
+    }
+
+    override fun getStatistics(): Statistics {
+        return CudaStatistics
+    }
+}
