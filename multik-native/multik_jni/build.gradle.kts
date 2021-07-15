98877f2814e9547d766e97ff2a87960da7fd37f0
devcrocod <devcrocod@gmail.com>
2021-07-15 21:37:02 +0300

    build multik_jni using gradle

---
+++ multik-native/multik_jni/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.internal.jvm.Jvm
+
+plugins {
+    `cpp-library`
+}
+
+apply(from = "$rootDir/gradle/openblas.gradle")
+
+val gccPath: String? = System.getenv("MinGW_x64_Bin_Path")
+
+library {
+    source.from(file("src/main/cpp"))
+
+    targetMachines.set(
+        listOf(
+            machines.linux.x86_64,
+            machines.windows.x86_64,
+            machines.macOS.x86_64
+        )
+    )
+
+    linkage.set(listOf(Linkage.SHARED))
+
+    binaries.configureEach {
+        if (gccPath != null) {
+            (toolChain as Gcc).path(gccPath)
+        }
+        compileTask.get().compilerArgs.addAll(compileTask.get().targetPlatform.map {
+            listOf(
+                "-std=c++14", "-O3", "-fno-exceptions", "-ffast-math",
+                "-I", "${Jvm.current().javaHome.canonicalPath}/include",
+                "-I", "$buildDir/openblas/include/"
+            ) + when {
+                it.operatingSystem.isMacOsX -> listOf("-I", "${Jvm.current().javaHome.canonicalPath}/include/darwin")
+                it.operatingSystem.isLinux -> listOf("-I", "${Jvm.current().javaHome.canonicalPath}/include/linux")
+                it.operatingSystem.isWindows -> listOf(
+                    "-static-libgcc", "-static-libstdc++", "-static", "-lpthread",
+                    "-I", "${Jvm.current().javaHome.canonicalPath}/include/win32"
+                )
+                else -> emptyList()
+            }
+        })
+    }
+
+
+}
+
+
+tasks.withType(CppCompile::class.java).configureEach { dependsOn("installOpenBlas") }
+
+tasks.withType(LinkSharedLibrary::class.java).configureEach { linkerArgs.addAll(listOf("$buildDir/openblas/lib/libopenblas.a")) }
