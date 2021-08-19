f3fec8ba40aa5e89e61780042c5affb3d90ac6b6
polka125 <novozhilov1999@mail.ru>
2021-08-20 01:12:45 +0300

    refactor qrShifted method

--- multik-jvm/src/main/kotlin/org/jetbrains/kotlinx/multik/jvm/linalg/eigen.kt
+++ multik-jvm/src/main/kotlin/org/jetbrains/kotlinx/multik/jvm/linalg/eigen.kt
@@ -1,13 +1,11 @@
 package org.jetbrains.kotlinx.multik.jvm.linalg
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.TypeCheckError
 import org.jetbrains.kotlinx.multik.api.*
 import org.jetbrains.kotlinx.multik.jvm.upperHessenberg
 import org.jetbrains.kotlinx.multik.ndarray.complex.Complex
 import org.jetbrains.kotlinx.multik.ndarray.complex.ComplexDouble
 import org.jetbrains.kotlinx.multik.ndarray.complex.ComplexFloat
 import org.jetbrains.kotlinx.multik.ndarray.data.*
-import org.jetbrains.kotlinx.multik.ndarray.operations.*
 import org.jetbrains.kotlinx.multik.ndarray.operations.timesAssign
 import java.lang.ArithmeticException
 import java.lang.UnsupportedOperationException
@@ -99,47 +97,47 @@ private fun qrShifted(a: MultiArray<ComplexDouble, D2>, trialsNumber: Int = 30):
     val itmax = trialsNumber * max(10, n)
     var kdefl = 0
 
-    var i = n
+    var i = n - 1
 
-    while (i >= 1) {
+    while (i >= 0) {
 
-        var l = 1
+        var l = 0
+
         var failedToConverge = true
 
         for (iteration in 0 until itmax) { // Look for a single small subdiagonal element
 
             l = run {
                 for (k in i downTo l + 1) {
-                    if (absL1(h[k - 1, k - 2]) < smlnum) {
+                    if (absL1(h[k, k - 1]) < smlnum) {
                         return@run k
                     }
 
-                    var tst = absL1(h[k - 2, k - 2]) + absL1(h[k - 1, k - 1])
+                    var tst = absL1(h[k - 1, k - 1]) + absL1(h[k, k])
 
                     if (tst == 0.0) {
-                        if (k - 2 >= 1) {
-                            tst += abs(h[k - 2, k - 3].re)
+                        if (k >= 2) {
+                            tst += abs(h[k - 1, k - 2].re)
                         }
-                        if (k + 1 <= n) {
-                            tst += abs(h[k, k - 1].re)
+                        if (k + 2 <= n) {
+                            tst += abs(h[k + 1, k].re)
                         }
                     }
 
-                    if (abs(h[k - 1, k - 2].re) <= ulp * tst) {
-
-                        // ==== The following is a conservative small subdiagonal
-                        // *           .    deflation criterion due to Ahues & Tisseur (LAWN 122,
-                        // *           .    1997). It has better mathematical foundation and
-                        // *           .    improves accuracy in some examples.  ====
-                        val ab = max(absL1(h[k - 1, k - 2]), absL1(h[k - 2, k - 1]))
-                        val ba = min(absL1(h[k - 1, k - 2]), absL1(h[k - 2, k - 1]))
+                    if (abs(h[k, k - 1].re) <= ulp * tst) {
+                        // The following is a conservative small subdiagonal
+                        // deflation criterion due to Ahues & Tisseur (LAWN 122,
+                        // 1997). It has better mathematical foundation and
+                        // improves accuracy in some examples.
+                        val ab = max(absL1(h[k, k - 1]), absL1(h[k - 1, k]))
+                        val ba = min(absL1(h[k, k - 1]), absL1(h[k - 1, k]))
                         val aa = max(
-                            absL1(h[k - 1, k - 1]),
-                            absL1(h[k - 2, k - 2] - h[k - 1, k - 1])
+                            absL1(h[k, k]),
+                            absL1(h[k - 1, k - 1] - h[k, k])
                         )
                         val bb = min(
-                            absL1(h[k - 1, k - 1]),
-                            absL1(h[k - 2, k - 2] - h[k - 1, k - 1])
+                            absL1(h[k, k]),
+                            absL1(h[k - 1, k - 1] - h[k, k])
                         )
 
                         val s = aa + ab
@@ -152,8 +150,8 @@ private fun qrShifted(a: MultiArray<ComplexDouble, D2>, trialsNumber: Int = 30):
                 return@run l
             }
 
-            if (l > 1) {
-                h[l - 1, l - 2] = ComplexDouble.zero
+            if (l > 0) {
+                h[l, l - 1] = ComplexDouble.zero
             }
 
             if (l >= i) {
@@ -171,19 +169,19 @@ private fun qrShifted(a: MultiArray<ComplexDouble, D2>, trialsNumber: Int = 30):
 
             when {
                 kdefl % (2 * kExceptionalShift) == 0 -> {
-                    s = dat1 * abs(h[i - 1, i - 2].re)
-                    t = h[i - 1, i - 1] + s
+                    s = dat1 * abs(h[i, i - 1].re)
+                    t = h[i, i] + s
                 }
                 kdefl % kExceptionalShift == 0 -> {
-                    s = dat1 * abs(h[l, l - 1].re)
-                    t = h[l - 1, l - 1] + s
+                    s = dat1 * abs(h[l + 1, l].re)
+                    t = h[l, l] + s
                 }
                 else -> {
-                    t = h[i - 1, i - 1]
-                    u = csqrt(h[i - 2, i - 1]) * csqrt(h[i - 1, i - 2])
+                    t = h[i, i]
+                    u = csqrt(h[i - 1, i]) * csqrt(h[i, i - 1])
                     s = absL1(u)
                     if (s != 0.0) {
-                        val x = (h[i - 2, i - 2] - t) * 0.5
+                        val x = (h[i - 1, i - 1] - t) * 0.5
                         val sx = absL1(x)
                         s = max(s, absL1(x))
                         var y = csqrt((x / s) * (x / s) + (u / s) * (u / s)) * s
@@ -202,18 +200,18 @@ private fun qrShifted(a: MultiArray<ComplexDouble, D2>, trialsNumber: Int = 30):
             var h21: ComplexDouble
 
             val m = run {
-                for (m in (i - 1) downTo l) {
-                    h11 = h[m - 1, m - 1]
-                    h22 = h[m, m]
+                for (m in i - 1 downTo l) {
+                    h11 = h[m, m]
+                    h22 = h[m + 1, m + 1]
                     h11s = h11 - t
-                    h21 = h[m, m - 1].re.toComplexDouble()
+                    h21 = h[m + 1, m].re.toComplexDouble()
                     s = absL1(h11s) + h21.abs()
                     h11s /= s
                     h21 /= s
                     v[0] = h11s
                     v[1] = h21
                     if (m > l) {
-                        val h10 = h[m - 1, m - 2]
+                        val h10 = h[m, m - 1]
 
                         if (h10.abs() * h21.abs() <= ulp * (absL1(h11s) * (absL1(h11) + absL1(h22)))) {
                             return@run m
@@ -226,70 +224,69 @@ private fun qrShifted(a: MultiArray<ComplexDouble, D2>, trialsNumber: Int = 30):
             // single-shift qr step
             for (k in m until i) {
                 if (k > m) {
-                    v[0] = h[k - 1, k - 2]
-                    v[1] = h[k, k - 2]
+                    v[0] = h[k, k - 1]
+                    v[1] = h[k + 1, k - 1]
                 }
 
-                // zlarfg( 2, v( 1 ), v( 2 ), 1, t1 )
                 val (v1, t1) = computeHouseholderReflectorInplace(2, v[0], v[1..2] as D1Array<ComplexDouble>)
                 v[0] = v1
 
                 if (k > m) {
-                    h[k - 1, k - 2] = v[0]
-                    h[k, k - 2] = ComplexDouble.zero
+                    h[k, k - 1] = v[0]
+                    h[k + 1, k - 1] = ComplexDouble.zero
                 }
 
                 val v2 = v[1]
                 val t2 = (t1 * v2).re
 
-                for (j in k..n) {
-                    val sum = t1.conjugate() * h[k - 1, j - 1] + h[k, j - 1] * t2
-                    h[k - 1, j - 1] -= sum
-                    h[k, j - 1] = h[k, j - 1] - sum * v2
+                for (j in k until n) {
+                    val sum = t1.conjugate() * h[k, j] + h[k + 1, j] * t2
+                    h[k, j] -= sum
+                    h[k + 1, j] = h[k + 1, j] - sum * v2
                 }
 
-                for (j in 1..min(k + 2, i)) {
-                    val sum = t1 * h[j - 1, k - 1] + h[j - 1, k] * t2
-                    h[j - 1, k - 1] -= sum
-                    h[j - 1, k] -= sum * v2.conjugate()
+                for (j in 0 until min(k + 2, i) + 1) {
+                    val sum = t1 * h[j, k] + h[j, k + 1] * t2
+                    h[j, k] -= sum
+                    h[j, k + 1] -= sum * v2.conjugate()
                 }
 
-                for (j in 1..n) {
-                    val sum = t1 * z[j - 1, k - 1] + z[j - 1, k] * t2
-                    z[j - 1, k - 1] -= sum
-                    z[j - 1, k] -= sum * v2.conjugate()
+                for (j in 0 until n) {
+                    val sum = t1 * z[j, k] + z[j, k + 1] * t2
+                    z[j, k] -= sum
+                    z[j, k + 1] -= sum * v2.conjugate()
                 }
 
                 if (k == m && m > l) {
                     var temp = ComplexDouble.one - t1
                     temp /= temp.abs()
-                    h[m, m - 1] = h[m, m - 1] * temp.conjugate()
+                    h[m + 1, m] = h[m + 1, m] * temp.conjugate()
                     if (m + 2 < i) {
-                        h[m + 1, m] *= temp
+                        h[m + 2, m + 1] *= temp
                     }
                     for (j in m..i) {
                         if (j != m + 1) {
-                            if (n > j) {
-                                (h[j - 1, j..n] as D1Array<ComplexDouble>) *= temp
+                            if (n > j + 1) {
+                                (h[j, (j + 1)..n] as D1Array<ComplexDouble>) *= temp
                             }
-                            h[0..(j - 1), j - 1] as D1Array<ComplexDouble> *= temp.conjugate()
-                            z[0..n, j - 1] as D1Array<ComplexDouble> *= temp.conjugate()
+                            h[0..j, j] as D1Array<ComplexDouble> *= temp.conjugate()
+                            z[0..n, j] as D1Array<ComplexDouble> *= temp.conjugate()
                         }
                     }
                 }
             }
 
             // Ensure that H(I,I-1) is real.
-            var temp = h[i - 1, i - 2]
+            var temp = h[i, i - 1]
             if (temp.im != 0.0) {
                 val rtemp = temp.abs().toComplexDouble()
-                h[i - 1, i - 2] = rtemp
+                h[i, i - 1] = rtemp
                 temp /= rtemp
-                if (n > i) {
-                    h[i - 1, i..n] as D1Array<ComplexDouble> *= temp.conjugate()
+                if (n > i + 1) {
+                    h[i, (i + 1)..n] as D1Array<ComplexDouble> *= temp.conjugate()
                 }
-                h[0..(i - 1), i - 1] as D1Array<ComplexDouble> *= temp
-                z[0..n, i - 1] as D1Array<ComplexDouble> *= temp
+                h[0..i, i] as D1Array<ComplexDouble> *= temp
+                z[0..n, i] as D1Array<ComplexDouble> *= temp
             }
         }
         if (failedToConverge) {
