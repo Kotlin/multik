25187ff23357577425936a85f674b5ec965ccc32
devcrocod <devcrocod@gmail.com>
2021-10-07 15:59:06 +0300

    fix link on linux

--- multik-native/multik_jni/build.gradle.kts
+++ multik-native/multik_jni/build.gradle.kts
@@ -13,10 +13,7 @@ apply(from = "$rootDir/gradle/openblas.gradle")
 val gccPath: String? = System.getenv("MinGW_x64_Bin_Path")
 val gfortranAndQuadmathPath: String? = System.getenv("path_to_gfortran")
 
-val linkList: List<String> = mutableListOf(
-    "$buildDir/openblas/lib/libopenblas.a",
-    "-static-libgcc", "-static-libstdc++", "-static", "-lpthread"
-).apply {
+val linkList: List<String> = mutableListOf("$buildDir/openblas/lib/libopenblas.a", "-static", "-lpthread").apply {
     if (gfortranAndQuadmathPath != null) {
         this.add("$gfortranAndQuadmathPath/libgfortran.a")
         this.add("$gfortranAndQuadmathPath/libquadmath.a")
@@ -62,14 +59,14 @@ library {
 tasks.withType(CppCompile::class.java).configureEach { dependsOn("installOpenBlas") }
 
 tasks.withType(LinkSharedLibrary::class.java).configureEach {
-    linkerArgs.addAll(linkList
-//        targetPlatform.map {
-//            linkList +
-//                if (it.operatingSystem.isWindows)
+    linkerArgs.addAll(
+        targetPlatform.map {
+            linkList +
+                if (it.operatingSystem.isWindows || it.operatingSystem.isMacOsX)
+                    listOf("-static-libgcc", "-static-libstdc++")
+                else
 //                    listOf("-static-libgcc", "-static-libstdc++", "-static", "-lpthread")
-//                else
-//                    listOf("-static-libgcc", "-static-libstdc++", "-static", "-lpthread")
-////                        emptyList()
-//        }
+                    emptyList()
+        }
     )
 }
