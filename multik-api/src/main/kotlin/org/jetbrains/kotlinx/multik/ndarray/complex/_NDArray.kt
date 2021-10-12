44fb1a172655c6e4f991ac6992e7bc7b910ea2fd
Benni Rogge 🦕 <benni.rogge@gmail.com>
2021-10-12 07:45:54 -0700

    adding re and im properties for arrays of complex numbers
    issue: https://github.com/Kotlin/multik/issues/53

---
+++ multik-api/src/main/kotlin/org/jetbrains/kotlinx/multik/ndarray/complex/_NDArray.kt
@@ -0,0 +1,49 @@
+package org.jetbrains.kotlinx.multik.ndarray.complex
+
+import org.jetbrains.kotlinx.multik.ndarray.data.Dimension
+import org.jetbrains.kotlinx.multik.ndarray.data.NDArray
+import org.jetbrains.kotlinx.multik.ndarray.operations.map
+
+/**
+ * Transforms this [NDArray] of [ComplexFloat] to an [NDArray] of the real part of complex numbers.
+ * Dimensions are preserved.
+ *
+ * @param D dimension.
+ * @return [NDArray] of real portion of [ComplexFloat]
+ */
+@get:JvmName("reFloat")
+public val <D : Dimension> NDArray<ComplexFloat, D>.re: NDArray<Float, D>
+    get() { return this.map { it.re } }
+
+/**
+ * Transforms this [NDArray] of [ComplexDouble] to an [NDArray] of the real part of complex numbers.
+ * Dimensions are preserved.
+ *
+ * @param D dimension.
+ * @return [NDArray] of real portion of [ComplexDouble]
+ */
+@get:JvmName("reDouble")
+public val <D : Dimension> NDArray<ComplexDouble, D>.re: NDArray<Double, D>
+    get() { return this.map { it.re } }
+
+/**
+ * Transforms this [NDArray] of [ComplexFloat] to an [NDArray] of the imaginary part of complex numbers.
+ * Dimensions are preserved.
+ *
+ * @param D dimension.
+ * @return [NDArray] of imaginary portion of [ComplexFloat]
+ */
+@get:JvmName("imFloat")
+public val <D : Dimension> NDArray<ComplexFloat, D>.im: NDArray<Float, D>
+    get() { return this.map { it.im } }
+
+/**
+ * Transforms this [NDArray] of [ComplexDouble] to an [NDArray] of the imaginary part of complex numbers.
+ * Dimensions are preserved.
+ *
+ * @param D dimension.
+ * @return [NDArray] of imaginary portion of [ComplexDouble]
+ */
+@get:JvmName("imDouble")
+public val <D : Dimension> NDArray<ComplexDouble, D>.im: NDArray<Double, D>
+    get() { return this.map { it.im } }
---
+++ multik-api/src/test/kotlin/org/jetbrains/kotlinx/multik/ndarray/complex/ComplexNDArrayTest.kt
@@ -0,0 +1,63 @@
+package org.jetbrains.kotlinx.multik.ndarray.complex
+
+import org.jetbrains.kotlinx.multik.api.d2arrayIndices
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarray
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ComplexNDArrayTest {
+
+    @Test
+    fun `re and im properties returns real and imaginary portion of float complex number array`() {
+        val complex = mk.d2arrayIndices(3, 3) { i, j -> ComplexFloat(i, j) }
+        val real = complex.re
+        val im = complex.im
+        val expectedReal = mk.d2arrayIndices(3, 3) { i, j -> i.toFloat() }
+        val expectedIm = mk.d2arrayIndices(3, 3) { i, j -> j.toFloat() }
+
+        assertEquals(complex.shape, real.shape)
+        assertEquals(complex.shape, im.shape)
+        assertEquals(expectedReal, real)
+        assertEquals(expectedIm, im)
+    }
+
+    @Test
+    fun `re and im properties returns real and imaginary portion of double complex number array`() {
+        val complex = mk.d2arrayIndices(3, 3) { i, j -> ComplexDouble(i, j) }
+        val real = complex.re
+        val im = complex.im
+        val expectedReal = mk.d2arrayIndices(3, 3) { i, j -> i.toDouble() }
+        val expectedIm = mk.d2arrayIndices(3, 3) { i, j -> j.toDouble() }
+
+        assertEquals(complex.shape, real.shape)
+        assertEquals(complex.shape, im.shape)
+        assertEquals(expectedReal, real)
+        assertEquals(expectedIm, im)
+    }
+
+    @Test
+    fun `re and im properties work for empty complex double arrays`() {
+        val complex = mk.ndarray(emptyList<ComplexDouble>())
+        val real = complex.re
+        val im = complex.im
+
+        assertEquals(complex.shape, real.shape)
+        assertEquals(complex.shape, im.shape)
+        assertTrue(real.isEmpty())
+        assertTrue(im.isEmpty())
+    }
+
+    @Test
+    fun `re and im properties work for empty complex float arrays`() {
+        val complex = mk.ndarray(emptyList<ComplexFloat>())
+        val real = complex.re
+        val im = complex.im
+
+        assertEquals(complex.shape, real.shape)
+        assertEquals(complex.shape, im.shape)
+        assertTrue(real.isEmpty())
+        assertTrue(im.isEmpty())
+    }
+}
