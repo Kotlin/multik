cecc88f2cb11bcd038470190d216def166ff6d0c
MarcusDunn <marcus.s.dunn@gmail.com>
2021-10-19 10:15:06 -0700

    added easy complex creation.

---
+++ multik-api/src/test/kotlin/org/jetbrains/kotlinx/multik/ndarray/complex/CreateComplexTest.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.kotlinx.multik.ndarray.complex
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class CreateComplexTest {
+
+    @Test
+    fun `test easy complex creation`() {
+        assertEquals(Complex.i(0x01.toDouble()), 0x01.i)
+        assertEquals(Complex.i(1.toShort().toDouble()), 1.toShort().i)
+        assertEquals(Complex.i(1.toDouble()), 1.i)
+        assertEquals(Complex.i(1L.toDouble()), 1L.i)
+        assertEquals(Complex.i(1f), 1f.i)
+        assertEquals(Complex.i(1.0), 1.0.i)
+
+        assertEquals(ComplexFloat(1, 1), 1 + 1f.i)
+        assertEquals(ComplexDouble(1, 1), 1 + 1.i)
+    }
+}
