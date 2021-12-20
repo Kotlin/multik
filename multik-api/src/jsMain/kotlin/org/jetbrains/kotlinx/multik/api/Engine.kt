a31c40b63b62de34ea2b5fedad7337d708259424
Luca Spinazzola <lms452@cornell.edu>
2021-12-20 16:44:30 -0500

    workaround to not cast enginesProvider

--- multik-api/src/jsMain/kotlin/org/jetbrains/kotlinx/multik/api/Engine.kt
+++ multik-api/src/jsMain/kotlin/org/jetbrains/kotlinx/multik/api/Engine.kt
@@ -1,12 +1,14 @@
 package org.jetbrains.kotlinx.multik.api
 
 
-public actual val enginesProvider : Map<EngineType, Engine> = HashMap()
+private val _enginesProvider : HashMap<EngineType, Engine> = HashMap()
 
+public actual val enginesProvider : Map<EngineType, Engine>
+    get() = _enginesProvider
 
 public actual fun initEnginesProvider(engines: List<Engine>) {
     engines.forEach {
-        (enginesProvider as HashMap)[it.type] = it
+        _enginesProvider[it.type] = it
     }
     Engine.loadEngine()
 }
--- multik-api/src/nativeCommonMain/kotlin/org/jetbrains/kotlinx/multik/api/Engine.kt
+++ multik-api/src/nativeCommonMain/kotlin/org/jetbrains/kotlinx/multik/api/Engine.kt
@@ -3,11 +3,14 @@ package org.jetbrains.kotlinx.multik.api
 import kotlin.native.concurrent.ThreadLocal
 
 @ThreadLocal
-public actual val enginesProvider : Map<EngineType, Engine> = HashMap()
+private val _enginesProvider : HashMap<EngineType, Engine> = HashMap()
 
+public actual val enginesProvider : Map<EngineType, Engine>
+    get() = _enginesProvider
 
 public actual fun initEnginesProvider(engines: List<Engine>) {
     engines.forEach {
-        (enginesProvider as HashMap)[it.type] = it
+        _enginesProvider[it.type] = it
     }
+    Engine.loadEngine()
 }
