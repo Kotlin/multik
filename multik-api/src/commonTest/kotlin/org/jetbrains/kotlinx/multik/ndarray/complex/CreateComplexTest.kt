9dfe0b928626109e247e8ecf470737671ad49fa0
devcrocod <devcrocod@gmail.com>
2022-01-18 14:31:56 +0300

    update test for complex numbers

--- multik-api/src/commonTest/kotlin/org/jetbrains/kotlinx/multik/ndarray/complex/CreateComplexTest.kt
+++ multik-api/src/commonTest/kotlin/org/jetbrains/kotlinx/multik/ndarray/complex/CreateComplexTest.kt
@@ -7,14 +7,9 @@ class CreateComplexTest {
 
     @Test
     fun test_easy_complex_creation() {
-        assertEquals(Complex.i(0x01.toDouble()), 0x01.i)
-        assertEquals(Complex.i(1.toShort().toDouble()), 1.toShort().i)
-        assertEquals(Complex.i(1.toDouble()), 1.i)
-        assertEquals(Complex.i(1L.toDouble()), 1L.i)
-        assertEquals(Complex.i(1f), 1f.i)
-        assertEquals(Complex.i(1.0), 1.0.i)
-
+        assertEquals(Complex.i(1.12f), 1.12f.i)
+        assertEquals(Complex.i(3.33), 3.33.i)
         assertEquals(ComplexFloat(1, 1), 1 + 1f.i)
-        assertEquals(ComplexDouble(1, 1), 1 + 1.i)
+        assertEquals(ComplexDouble(3, 7), 3 + 7.0.i)
     }
 }
