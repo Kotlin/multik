d405ba94daeb75eb96ede9529bbe3c533a9c3457
devcrocod <devcrocod@gmail.com>
2022-07-16 01:15:01 +0200

    add naive impl of pow

--- multik-default/src/jsMain/kotlin/org.jetbrains.kotlinx.multik.default/linalg/DefaultLinAlg.kt
+++ multik-default/src/jsMain/kotlin/org.jetbrains.kotlinx.multik.default/linalg/DefaultLinAlg.kt
@@ -6,15 +6,17 @@ package org.jetbrains.kotlinx.multik.default.linalg
 
 import org.jetbrains.kotlinx.multik.api.linalg.LinAlg
 import org.jetbrains.kotlinx.multik.api.linalg.LinAlgEx
+import org.jetbrains.kotlinx.multik.kotlin.linalg.KELinAlg
 import org.jetbrains.kotlinx.multik.ndarray.data.D2
 import org.jetbrains.kotlinx.multik.ndarray.data.MultiArray
 import org.jetbrains.kotlinx.multik.ndarray.data.NDArray
 
-public expect object DefaultLinAlg : LinAlg {
+public actual object DefaultLinAlg : LinAlg {
 
-    override val linAlgEx: LinAlgEx
+    actual override val linAlgEx: LinAlgEx
+        get() = DefaultLinAlgEx
 
-    override fun <T : Number> pow(mat: MultiArray<T, D2>, n: Int): NDArray<T, D2>
+    actual override fun <T : Number> pow(mat: MultiArray<T, D2>, n: Int): NDArray<T, D2> = KELinAlg.pow(mat, n)
 
-    override fun <T : Number> norm(mat: MultiArray<T, D2>, p: Int): Double
+    actual override fun <T : Number> norm(mat: MultiArray<T, D2>, p: Int): Double = KELinAlg.norm(mat, p)
 }
