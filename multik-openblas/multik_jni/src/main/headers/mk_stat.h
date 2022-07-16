ea48d9c5c60290c05b13c3688e43029e081a086b
devcrocod <devcrocod@gmail.com>
2022-07-17 01:59:11 +0200

    add stdint for mingw

--- multik-openblas/multik_jni/src/main/headers/mk_stat.h
+++ multik-openblas/multik_jni/src/main/headers/mk_stat.h
@@ -2,6 +2,8 @@
  * Copyright 2020-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
+#include <stdint.h>
+
 #ifndef MULTIK_JNI_MULTIK_JNI_SRC_MAIN_HEADERS_MK_STAT_H_
 #define MULTIK_JNI_MULTIK_JNI_SRC_MAIN_HEADERS_MK_STAT_H_
 #ifdef __cplusplus
