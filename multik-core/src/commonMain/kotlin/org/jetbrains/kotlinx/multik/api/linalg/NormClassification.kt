9e6ba481ccf2c6cd6eaaa9c5d5fc1b9c634beb94
devcrocod <devcrocod@gmail.com>
2022-07-22 01:15:09 +0200

    fix kdoc for Norm

--- multik-core/src/commonMain/kotlin/org/jetbrains/kotlinx/multik/api/linalg/NormClassification.kt
+++ multik-core/src/commonMain/kotlin/org/jetbrains/kotlinx/multik/api/linalg/NormClassification.kt
@@ -7,15 +7,26 @@ package org.jetbrains.kotlinx.multik.api.linalg
 /**
  * Matrix norm types.
  *
- * @param lapackCode value for lapack
- * @property Max max(abs(A(i,j)))
- * @property N1 denotes the  one norm of a matrix (maximum column sum)
- * @property Inf denotes the  infinity norm  of a matrix  (maximum row sum)
- * @property Fro denotes the  Frobenius norm of a matrix (square root of sum of squares)
+ * @property lapackCode value for lapack
  */
 public enum class Norm(public val lapackCode: Char) {
+    /**
+     * max(abs(A(i,j)))
+     */
     Max('M'),
+
+    /**
+     * denotes the  one norm of a matrix (maximum column sum)
+     */
     N1('1'),
+
+    /**
+     * denotes the  infinity norm  of a matrix  (maximum row sum)
+     */
     Inf('I'),
+
+    /**
+     * denotes the  Frobenius norm of a matrix (square root of sum of squares)
+     */
     Fro('F')
 }
