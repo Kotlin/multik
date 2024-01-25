c1bb21edb516ef30b43386813ecf48ddf94a7ad9
devcrocod <devcrocod@gmail.com>
2024-01-25 23:24:26 +0100

    Move openblas tests to commonTest directory
    Add linear algebra, mathematics, statistics, and maths test for kotlin/native

--- multik-openblas/src/commonTest/kotlin/org/jetbrains/kotlinx/multik/openblas/linalg/NativeLinAlgTest.kt
+++ multik-openblas/src/commonTest/kotlin/org/jetbrains/kotlinx/multik/openblas/linalg/NativeLinAlgTest.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2020-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package org.jetbrains.kotlinx.multik.openblas.linalg
 
 import org.jetbrains.kotlinx.multik.api.*
