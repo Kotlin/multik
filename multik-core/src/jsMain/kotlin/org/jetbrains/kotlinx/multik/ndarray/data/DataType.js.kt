401bc2f68c43958abf65166b18603eb5fbe5cc96
devcrocod <devcrocod@gmail.com>
2026-02-27 23:05:41 +0100

    docs: update KDocs for JS platform engine registry and dataTypeOf

--- multik-core/src/jsMain/kotlin/org/jetbrains/kotlinx/multik/ndarray/data/DataType.js.kt
+++ multik-core/src/jsMain/kotlin/org/jetbrains/kotlinx/multik/ndarray/data/DataType.js.kt
@@ -6,6 +6,7 @@ import org.jetbrains.kotlinx.multik.ndarray.complex.JsComplexDouble
 import org.jetbrains.kotlinx.multik.ndarray.data.DataType.*
 import kotlin.reflect.KClass
 
+/** Resolves the [DataType] for the given [KClass]. Maps JS-specific complex types including [JsComplexDouble]. */
 @PublishedApi
 @Suppress("NOTHING_TO_INLINE")
 internal actual inline fun <T : Any> dataTypeOf(type: KClass<out T>): DataType =
